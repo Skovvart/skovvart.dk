a6ff375f01c12f9b4e2a45bc7cad56bf3d4441fa
Nicolai Skovvart <skovvart@gmail.com>
2020-10-06 00:09:38 +0200

    IIS 404 handling

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,3 @@ const sveltePreprocess = require('svelte-preprocess');
 module.exports = {
     preprocess: sveltePreprocess(),
 };
-/*
-{
-        scss: {
-            includePaths: ['src'],
-        },
-        postcss: {
-            plugins: [require('autoprefixer')],
-        },
-    }
-*/
